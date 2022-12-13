4a310fe9bd11fbc0072e053caca509694c494fdc
Mike <michaelmarracq@yahoo.com>
2022-12-12 16:36:09 -0800

    added 18.2 exercise

---
+++ Exercises/python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,16 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    dict = {}
+    for let in phrase:
+        if let in dict:
+            dict[let] += 1
+        else:
+            dict[let] = 1
+    return dict
